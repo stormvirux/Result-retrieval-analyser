4a26990edeb2d1f61eda523c69587ffdafc851bd
Thaha Muhammed <mthaha1989@gmail.com>
2014-03-13 13:09:14 +0530

    Removed yearouts from the list

--- analysis.py
+++ analysis.py
@@ -1,15 +1,27 @@
 import pandas as pd
 import numpy
-import matplotlib.pyplot
-pd.set_option('max_columns', 50)
+import matplotlib.pyplot as plt
+from scipy import stats
+import statsmodels.api as sm
+
+#pd.set_option('max_columns', 50)
 data=pd.read_csv("output11cs.csv")
 #df2 = data.groupby(['Total','Total.1','Total.2','Total.3','Total.4','Total.5','Total.6','Total.7'])['Main Total'].sum().reset_index()
 #df2.sum(axis=1)
 df3=data[['Total','Total.1','Total.2','Total.3','Total.4','Total.5','Total.6','Total.7']]
+print df3
 data["Main Total"]=df3.sum(axis=1)
 data = data.dropna()
 data.reset_index(drop=True)
+x=data["Main Total"]
 print data["Main Total"]
+k_test=stats.kstest(x,"norm")
+print sm.stats.lillifors(x)
+print k_test
 #print df2
 #print df2.mean()
-
+"""plt.hist(data["Main Total"])
+plt.title("Gaussian Histogram")
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+plt.show()"""
--- rra.py
+++ rra.py
@@ -43,19 +43,21 @@ def getval():
 		soup=BeautifulSoup(page_html)
 		soup.prettify()
 		fl = codecs.open('output'+sys.argv[1]+sys.argv[2]+'.csv', 'ab',encoding="Utf-8")
-		record=[texts.text for texts in soup.findAll("td",{"align":"center"})]
+		record=[texts.text for texts in soup.findAll('td',{"align":"center"})]
 		"""for y in soup.findAll("td"):
 			if y.parent.name=="tr":
 				lol.append(y.text)
 		if lol[-10] == "A" or lol[-10]=="P" or lol[-10]=="F":
 			lol[-10],lol[-1]=lol[-1],lol[-10]"""
+		del record[0:4]
 		for y in record:
 			if "P" in y: record.remove("P")
 			elif "F" in y: record.remove("F")
 			elif "A" in y: record.remove("A")
-		del record[0:4]
+				
 		if len(record)>24:
-			del record[24:]	
+			del record[24:]
+		if "Total" in record: del record[:]	
 		if record:
 			fl.write("\n"+usnl[x]+",")
 			for y in record:
@@ -88,7 +90,7 @@ def ret():
 	x=0
 	year=sys.argv[1]
 	branches=sys.argv[2]
-	for rno in range(1,20):
+	for rno in range(1,120):
 		usn="4pa"+year+branches+"%03d"%rno
 		payload={'rid':usn,'submit':'submit'}
 		r=requests.post("http://results.vtu.ac.in/vitavi.php/post",data=payload)
