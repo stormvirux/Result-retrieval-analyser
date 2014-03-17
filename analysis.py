dfb8d5d85a6c21da0c1b8ebac2fcb00b6cc512ea
Thaha Muhammed <mthaha1989@gmail.com>
2014-03-17 12:21:16 +0530

    compare all marks histogram

--- analysis.py
+++ analysis.py
@@ -7,10 +7,11 @@ from numpy.random import randn
 import matplotlib as mpl
 import seaborn as sns
 
+sns.set_color_palette("deep", desat=.6)
+mpl.rc("figure", figsize=(8, 4))
+
 #pd.set_option('max_columns', 50)
 data=pd.read_csv("output11cs.csv")
-#df2 = data.groupby(['Total','Total.1','Total.2','Total.3','Total.4','Total.5','Total.6','Total.7'])['Main Total'].sum().reset_index()
-#df2.sum(axis=1)
 df3=data[['Total','Total.1','Total.2','Total.3','Total.4','Total.5','Total.6','Total.7']]
 data["Main Total"]=df3.sum(axis=1)
 data = data.dropna()
@@ -20,12 +21,14 @@ y = stats.uniform.rvs(size = 100)
 print stats.kstest(x,'norm')
 print stats.normaltest(x)
 print sm.stats.lillifors(x)
-#print df2
-print df3.mean()
 #plt.hist(x, 70, histtype="stepfilled", alpha=.7);
 #max_data = np.r_[data["Main Total"]].max()
 #bins = np.linspace(0, max_data, max_data + 1)
-plt.hist(data["Main Total"],histtype="stepfilled", alpha=.7)
+#plt.hist(data["Main Total"],normed=True,alpha=.7)
+plt.hist(data["Total.1"],20,normed=True, color="#6495ED", alpha=.5)
+plt.hist(data['Total.2'],20, normed=True, color="#F08080", alpha=.5)
+#sns.lmplot("Total.1", "Total", data)
+#sns.kdeplot(data['Main Total'], shade=True)
 plt.title("Total marks Histogram")
 plt.xlabel("Value")
 plt.ylabel("Frequency")
@@ -36,3 +39,5 @@ t_data = pd.DataFrame([[key,dick[key]] for key in dick], columns=['lol','rofl'])
 plt.axis([200, 800, 0, 2])
 plt.plot(t_data["lol"],t_data["rofl"],'bs' )
 plt.show()"""
+
+
---
+++ asys.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+import statsmodels.api as sm
+from numpy.random import randn
+import matplotlib as mpl
+import seaborn as sns
+
+def compSub():
+	
