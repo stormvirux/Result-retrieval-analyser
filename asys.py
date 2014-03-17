402d0638ca142d97b5ca2b519f370ccd4adb1f80
Thaha Muhammed <mthaha1989@gmail.com>
2014-03-17 16:14:22 +0530

    done with barplot

--- asys.py
+++ asys.py
@@ -10,5 +10,47 @@ from numpy.random import randn
 import matplotlib as mpl
 import seaborn as sns
 
-def compSub():
+sns.set_color_palette("deep", desat=.6)
+mpl.rc("figure", figsize=(8, 4))
+
+def Compavg():
+	markMax=[]
+	markAvg=[]
+	N = 5
+	ind = np.arange(N)    
+	width = 0.35 
+	fig = plt.figure()
+	ax = fig.add_subplot(111)      
+	markMax.extend((data["Total"].max(),data["Total.1"].max(),data["Total.2"].max(),data["Total.3"].max(),data["Total.4"].max()))
+	markAvg.extend((data["Total"].mean(),data["Total.1"].mean(),data["Total.2"].mean(),data["Total.3"].mean(),data["Total.4"].mean()))
+	rects1 = ax.bar(ind, markMax, width, color='black')
+	rects2 = ax.bar(ind+width, markAvg, width, color='green')
+	ax.set_xlim(-width,len(ind)+width)
+	ax.set_ylim(0,120)
+	ax.set_ylabel('Marks')
+	ax.set_title('Max, Mean and Your Marks')
+	xTickMarks = ['Subject'+str(i) for i in range(1,6)]
+	ax.set_xticks(ind+width)
+	xtickNames = ax.set_xticklabels(xTickMarks)
+	plt.setp(xtickNames, rotation=10, fontsize=10)
+	ax.legend( (rects1[0], rects2[0]), ('Max', 'Mean') )
+	plt.show()
 	
+def compSub():
+	plt.hist(data["Total.1"],20,normed=True, color="#6495ED", alpha=.7)
+	plt.hist(data['Total.2'],20, normed=True, color="#F08080", alpha=.7)
+	plt.hist(data['Total.3'],20, normed=True, color="#8C1515", alpha=.7)
+	plt.hist(data['Total.4'],20, normed=True, color="#D2C295", alpha=.7)
+	plt.hist(data['Total.5'],20, normed=True, color="#4168B7", alpha=.7)
+	plt.title("Total marks Histogram")
+	plt.xlabel("Value")
+	plt.ylabel("Frequency")
+	plt.show()
+
+data=pd.read_csv("output11cs.csv")
+df3=data[['Total','Total.1','Total.2','Total.3','Total.4','Total.5','Total.6','Total.7']]
+data["Main Total"]=df3.sum(axis=1)
+data = data.dropna()
+data.reset_index(drop=True)
+#Compavg()
+compSub()
